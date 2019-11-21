b71ac0f8a22355003c85ff2f12e6ee5e8c3f8f30
Caleb Cartwright <caleb.cartwright@outlook.com>
2019-11-20 20:22:54 -0600

    tests: update jest config and test scripts

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,38 @@
 module.exports = {
-    coverageDirectory: './coverage/',
+    globals: {
+        'ts-jest': {
+            'tsConfig': 'tsconfig.json'
+        }
+    },
+    coverageDirectory: '.coverage/unit',
     coveragePathIgnorePatterns: [
         '/node_modules/'
     ],
     collectCoverageFrom: [
         'src/**/*.ts'
     ],
-    globals: {
-        'ts-jest': {
-            'tsConfig': 'tsconfig.json'
+    coverageReporters: [
+        'html',
+        'lcov',
+        'cobertura',
+        'text',
+        'text-summary'
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
         }
     },
+
     reporters: [
         'default',
         [
             'jest-junit',
             {
-                outputDirectory: '.testresults',
+                outputDirectory: '.testresults/unit',
                 outputName: 'junit.xml'
             }
         ]
